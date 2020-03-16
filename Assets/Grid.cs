fa5c8e37f4be7f1b6c5863309350b0f9ddb00984
RaimondZoc <37068786+RaimondZoc@users.noreply.github.com>
2020-03-16 19:54:21 +0200

    Initial commit
    A grid is now generated on y and x parameters

---
+++ Assets/Grid.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class Grid : MonoBehaviour
+{
+    [SerializeField]
+    private int xSize, ySize;
+
+    private Mesh mesh;
+    private Vector3[] gridVertices;
+
+    void Start()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+        StartCoroutine("Generate");
+    }
+
+    private IEnumerator Generate()
+    {
+
+        gridVertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        for (int i = 0, y = 0; y <= ySize; y++)
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                gridVertices[i] = new Vector3(x, y);
+                yield return new WaitForSeconds(0.1f);
+            }
+
+        mesh.vertices = gridVertices;
+
+        int[] triangles = new int[6 * xSize * ySize];
+        for (int ti = 0, y = 0; y < ySize; y++)
+            for (int x = 0; x < xSize; ti += 6, x++) 
+       {
+            triangles[ti] = y * (xSize + 1) + x;
+            triangles[ti + 1] = triangles[ti + 4] = y * (xSize + 1) + (xSize + 1) + x; 
+            triangles[ti + 2] = triangles[ti + 3] = y * (xSize + 1) + x + 1;
+            triangles[ti + 5] = y * (xSize + 1) + xSize + (x + 2);
+            mesh.triangles = triangles;
+            yield return new WaitForSeconds(0.5f);
+        }
+        Debug.Log(mesh.triangles.Length);
+
+        yield return 0;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (gridVertices != null)
+            for (int i = 0; i < gridVertices.Length; i++)
+        Gizmos.DrawSphere(gridVertices[i], 0.1f);
+    }
+
+
+}
